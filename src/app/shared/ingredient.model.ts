488bae27016209280f8d497e0715fbada927c55e
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-26 11:52:50 +0100

    adding ingredient model in shared folder

---
+++ src/app/shared/ingredient.model.ts
@@ -0,0 +1,18 @@
+import { Ingredient } from './ingredient.model';
+/*
+export class Ingredient {
+  public name: string;
+  public amount: number;
+
+  constructor(name: string, amount: number) {
+    this.name = name;
+    this.amount = amount;
+  }
+}
+*/
+
+// or shortcut
+
+export class Ingredient {
+  constructor(public name: string, public amount: number) {}
+}
