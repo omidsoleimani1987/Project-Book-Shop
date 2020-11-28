1940964eaabc0d48b6cc6fca66d7c0fd302e9886
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-28 15:53:04 +0100

    working on recipe detail component to show by clicking

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 import { Recipe } from '../../shared/recipe.model';
 
@@ -14,9 +14,19 @@ export class RecipeListComponent implements OnInit {
       'this is a test',
       'https://images.squarespace-cdn.com/content/v1/53861df2e4b03d58a4690cff/1406686608383-YSZYPCOLOZFW1PMBW92D/ke17ZwdGBToddI8pDm48kMnHRrpWSlSCB9XabjgyantZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpz4FkPJA585QYBg9EjPvRpkIxxB_SdP_xzqtxyFDizV5aadH9FfX0OATazAJGndjd8/6546676667_e406be7c0f_o.jpg?format=500w'
     ),
+    new Recipe(
+      'another recipe',
+      'this is a good food',
+      'https://hips.hearstapps.com/delish/assets/17/39/1506456246-delish-healthy-chicken-casserole-1.jpg'
+    ),
   ];
 
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
   constructor() {}
 
   ngOnInit(): void {}
+
+  onRecipeSelected(recipeItem: Recipe): void {
+    this.recipeWasSelected.emit(recipeItem);
+  }
 }
