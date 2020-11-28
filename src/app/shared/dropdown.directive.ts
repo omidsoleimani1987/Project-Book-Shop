d7ff2ca9380a07a5aeeb0fde345c0f1fe46b2347
omidsoleimani <o.soleimani.ie@gmail.com>
2020-11-28 23:56:00 +0100

    adding dropdown directive to toggle the open class

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,24 @@
+import { Directive, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]',
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  @HostListener('click') toggleOpenClass(): void {
+    this.isOpen = !this.isOpen;
+  }
+
+  constructor() {}
+
+  /*
+  close the dropdown by clicking any where on screen
+
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+  constructor(private elRef: ElementRef) {}
+
+  */
+}
