662bab6056350b7b53d2ccc3d112c57a2ab467d6
omidsoleimani <o.soleimani.ie@gmail.com>
2020-12-01 23:05:44 +0100

    adding route navigation

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -2,14 +2,8 @@ import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-header',
-  templateUrl: './header.component.html',
+  templateUrl: './header.component.html'
 })
 export class HeaderComponent {
   collapsed = true;
-
-  @Output() selectedFeature = new EventEmitter<string>();
-
-  onSelect(activeFeature: string): void {
-    this.selectedFeature.emit(activeFeature);
-  }
 }
