a33458a4f9e83394b1bf968f61b53c3d2037d45d
omidsoleimani <o.soleimani.ie@gmail.com>
2020-12-07 22:55:40 +0100

    transforming the response data

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -11,26 +11,28 @@ import { ShoppingListService } from '../shopping-list/shopping-list.service';
 export class RecipeService {
   recipeChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe(
-      'Tasty Schnitzel',
-      'A super tasty Schnitzel',
-      'https://i3-img.kabeleins.de/pis/ezone/b85cqgELB38wdEB0AB1fHPDQCtTDCJ4UYl_Ic-IXCoYylZ0mXbP3Z8_B4JBo6LCimhDa22AtrTM8DB_M9Sv4e9OM8slbXthgEbE4ubOs1rnC2jOcxZ9a-oZ09bK01Tt0b1vpGmsgVYDky7gxWa9Ji7hMq0kR_pQ2jRtK2fQmW4rytj1AFXX-l6q3zvzGC0WR4MrZm2eVebI8dVElyIxxoffiauzSGJCqKiLU64nsO3YYm77aW-CmLiBxue3Xb7cEvMfmUM1lzYjwCbSan5Q7lQCikI2SfI2G/profile:mag-996x562',
-      [new Ingredient('Meat', 2), new Ingredient('Tomato', 4)]
-    ),
-    new Recipe(
-      'Big Fat Burger',
-      'what else you need to have?',
-      'https://nebula.wsimg.com/343f16710d2a7d2b8ba4f51115a95f67?AccessKeyId=531592D248B589D87A56&disposition=0&alloworigin=1',
-      [new Ingredient('Meat', 4), new Ingredient('Bread', 2)]
-    ),
-    new Recipe(
-      'Spaghetti',
-      'no one can resists!',
-      'https://i0.wp.com/www.eatthis.com/wp-content/uploads/2019/01/healthy-spaghetti-spicy-tomato-sauce.jpg?fit=1200%2C879&ssl=1',
-      [new Ingredient('Meat', 4), new Ingredient('Bread', 2)]
-    )
-  ];
+  // private recipes: Recipe[] = [
+  //   new Recipe(
+  //     'Tasty Schnitzel',
+  //     'A super tasty Schnitzel',
+  //     'https://i3-img.kabeleins.de/pis/ezone/b85cqgELB38wdEB0AB1fHPDQCtTDCJ4UYl_Ic-IXCoYylZ0mXbP3Z8_B4JBo6LCimhDa22AtrTM8DB_M9Sv4e9OM8slbXthgEbE4ubOs1rnC2jOcxZ9a-oZ09bK01Tt0b1vpGmsgVYDky7gxWa9Ji7hMq0kR_pQ2jRtK2fQmW4rytj1AFXX-l6q3zvzGC0WR4MrZm2eVebI8dVElyIxxoffiauzSGJCqKiLU64nsO3YYm77aW-CmLiBxue3Xb7cEvMfmUM1lzYjwCbSan5Q7lQCikI2SfI2G/profile:mag-996x562',
+  //     [new Ingredient('Meat', 2), new Ingredient('Tomato', 4)]
+  //   ),
+  //   new Recipe(
+  //     'Big Fat Burger',
+  //     'what else you need to have?',
+  //     'https://nebula.wsimg.com/343f16710d2a7d2b8ba4f51115a95f67?AccessKeyId=531592D248B589D87A56&disposition=0&alloworigin=1',
+  //     [new Ingredient('Meat', 4), new Ingredient('Bread', 2)]
+  //   ),
+  //   new Recipe(
+  //     'Spaghetti',
+  //     'no one can resists!',
+  //     'https://i0.wp.com/www.eatthis.com/wp-content/uploads/2019/01/healthy-spaghetti-spicy-tomato-sauce.jpg?fit=1200%2C879&ssl=1',
+  //     [new Ingredient('Meat', 4), new Ingredient('Bread', 2)]
+  //   )
+  // ];
+
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {}
 
--- src/app/shared/services/data-storage.service.ts
+++ src/app/shared/services/data-storage.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
 
 import { Recipe } from './../models/recipe.model';
 
@@ -29,6 +30,17 @@ export class DataStorageService {
     this.http
       .get<Recipe[]>(
         'https://recipe-book-cf05a-default-rtdb.firebaseio.com/recipes.json'
+      )
+      .pipe(
+        map(recipes => {
+          // to check if all fetched recipe items have the property named ingredients
+          return recipes.map(recipe => {
+            return {
+              ...recipe,
+              ingredients: recipe.ingredients ? recipe.ingredients : []
+            };
+          });
+        })
       )
       .subscribe(responseData => {
         this.recipeService.setRecipes(responseData);
