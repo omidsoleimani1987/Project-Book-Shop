b9627d1fc09a372f59caa3085265325667704dfe
omidsoleimani <o.soleimani.ie@gmail.com>
2020-12-08 14:02:46 +0100

    Preparing the Signup Request

---
+++ src/app/auth/auth.service.ts
@@ -0,0 +1,37 @@
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+
+// define the response type
+interface AuthResponseData {
+  idToken: string;
+  email: string;
+  refreshToken: string;
+  expiresIn: string;
+  localId: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  constructor(private http: HttpClient) {}
+
+  signup(
+    userEmail: string,
+    userPassword: string
+  ): Observable<AuthResponseData> {
+    return this.http.post<AuthResponseData>(
+      'https://identitytoolkit.googleapis.com/v1/accounts:signUp',
+      {
+        email: userEmail,
+        password: userPassword,
+        returnSecureToken: true
+      },
+      {
+        params: new HttpParams().set(
+          'key',
+          'AIzaSyAuz8VDqXqpf3pYsQNsrqezQ2SQwv96t-c'
+        )
+      }
+    );
+  }
+}
