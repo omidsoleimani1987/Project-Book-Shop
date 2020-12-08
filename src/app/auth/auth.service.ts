116cb704ef2f1dd35635218c53d8768787688a93
omidsoleimani <o.soleimani.ie@gmail.com>
2020-12-08 19:06:40 +0100

    Login Error Handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,5 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpParams } from '@angular/common/http';
+import {
+  HttpClient,
+  HttpErrorResponse,
+  HttpParams
+} from '@angular/common/http';
 
 import { catchError } from 'rxjs/operators';
 import { Observable, throwError } from 'rxjs';
@@ -17,6 +21,48 @@ export interface AuthResponseData {
 @Injectable({ providedIn: 'root' })
 export class AuthService {
   constructor(private http: HttpClient) {}
+
+  private handleError(responseError: HttpErrorResponse): Observable<never> {
+    let errorMessage = 'An Unknown Error Occurred';
+
+    if (!responseError.error || !responseError.error.error) {
+      return throwError(errorMessage);
+    }
+
+    switch (responseError.error.error.message) {
+      // sign up error cases
+      case 'EMAIL_EXISTS':
+        errorMessage = 'This email address already exists.';
+        break;
+
+      case 'OPERATION_NOT_ALLOWED':
+        errorMessage = 'Password sign-in is disabled for this project.';
+        break;
+
+      case 'TOO_MANY_ATTEMPTS_TRY_LATER':
+        errorMessage =
+          'We have blocked all requests from this device due to unusual activity. Try again later.';
+        break;
+
+      // login error cases
+      case 'EMAIL_NOT_FOUND':
+        errorMessage =
+          'There is no user record corresponding to this identifier. The user may have been deleted.';
+        break;
+
+      case 'INVALID_PASSWORD':
+        errorMessage =
+          'The password is invalid or the user does not have a password.';
+        break;
+
+      case 'USER_DISABLED':
+        errorMessage =
+          'The user account has been disabled by an administrator.';
+        break;
+    }
+
+    return throwError(errorMessage);
+  }
 
   signup(
     userEmail: string,
@@ -37,41 +83,25 @@ export class AuthService {
           )
         }
       )
-      .pipe(
-        catchError(responseError => {
-          let errorMessage = 'An Unknown Error Occurred';
-
-          if (!responseError.error || !responseError.error.error) {
-            return throwError(errorMessage);
-          }
-
-          switch (responseError.error.error.message) {
-            case 'EMAIL_EXISTS':
-              errorMessage = 'This email address already exists!';
-
-            // case 'OPERATION_NOT_ALLOWED':
-            // errorMessage = 'Password sign-in is disabled for this project';
-          }
-
-          return throwError(errorMessage);
-        })
-      );
+      .pipe(catchError(this.handleError));
   }
 
   login(userEmail: string, userPassword: string): Observable<AuthResponseData> {
-    return this.http.post<AuthResponseData>(
-      'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
-      {
-        email: userEmail,
-        password: userPassword,
-        returnSecureToken: true
-      },
-      {
-        params: new HttpParams().set(
-          'key',
-          'AIzaSyAuz8VDqXqpf3pYsQNsrqezQ2SQwv96t-c'
-        )
-      }
-    );
+    return this.http
+      .post<AuthResponseData>(
+        'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword',
+        {
+          email: userEmail,
+          password: userPassword,
+          returnSecureToken: true
+        },
+        {
+          params: new HttpParams().set(
+            'key',
+            'AIzaSyAuz8VDqXqpf3pYsQNsrqezQ2SQwv96t-c'
+          )
+        }
+      )
+      .pipe(catchError(this.handleError));
   }
 }
